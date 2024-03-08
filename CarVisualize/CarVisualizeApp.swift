7eded0d935a250762c6374883c1a9375d43fc4e9
Andre <andre_fabriciow@hotmail.com>
2024-03-07 21:13:22 -0300

    Star project

---
+++ CarVisualize/CarVisualizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarVisualizeApp.swift
+//  CarVisualize
+//
+//  Created by André Wozniack on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CarVisualizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
